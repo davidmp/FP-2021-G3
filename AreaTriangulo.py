d7ac9ebefb31339e053aca1dc1bc63d8493b0620
davidmamani <>
2021-04-26 16:45:08 +0000

    8 lenguajes de programacion

---
+++ AreaTriangulo.py
@@ -0,0 +1,9 @@
+#Definicion de variables y otros
+print("Ajercicio 01: Area de un Triangulo ")
+#Datos de entrada mediante dispositivos de entrada
+b=int(input("Ingrese Base:"))
+h=int(input("Ingrese Altura:"))
+#Proceso
+area=(b*h)/2
+#Datos de Salida en dispositivos de Salida
+print("Area de Triangulo es:", area)
