acf0866bb23207f51f895b484cab18e5ec9c0069
davidmamani <>
2021-05-05 19:45:50 +0000

    Ejemplo 3.8

--- structure-conditional/EstCondicional.py
+++ structure-conditional/EstCondicional.py
@@ -26,6 +26,33 @@ def estCondicional02():
     montoP=cantidadX*75
   #Datos de salida
   print("La cantidad a pagar es:", montoP)
+def votoElecciones():
+  print ("Como saber si puedes votar por tu edad")
+  mensaje=""
+  edadP=int(input("ingrese la edad que tiene:"))
+  if edadP>=18:
+    mensaje="Usted tiene la edad necesaria para votar"
+  else:
+    mensaje="Usted no cumple con la edad minima para votar"
+  print(mensaje) 
+
+def pagoSemanaBase40horas():
+  print ("Pago semanal del trabajador")
+  sueldoPagarSem=0.0
+  #Datos de entrada
+  horasTra=int(input("Ingrese horas trabajadas a la semana:"))
+  horasPago=int(input("Ingrese el pago por hora:")) 
+  #Proceso 
+  if horasTra>40:
+    sueldoPagarSem=40*horasPago+(horasTra-40)*2*horasPago
+  else:
+    sueldoPagarSem=horasTra*horasPago
+  #Datos de salida
+  print("El sueldo a pagar al trabajador es:", sueldoPagarSem)
+
+
 
 #estCondicional01()
-estCondicional02()
+#estCondicional02()
+#votoElecciones()
+pagoSemanaBase40horas()
