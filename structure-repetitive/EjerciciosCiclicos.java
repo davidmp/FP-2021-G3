1744ddc901b0bebbb058ddb60ad9051afadb1150
davidmp <mamanipari@gmail.com>
2021-05-19 15:13:13 -0500

    Ejemplo 4.7-4.2

--- structure-repetitive/EjerciciosCiclicos.java
+++ structure-repetitive/EjerciciosCiclicos.java
@@ -18,7 +18,7 @@ public class EjerciciosCiclicos {
         for (int numero=1; numero<=nNumeros; numero++) {
             /*System.out.print("Ingrese el valor de la posicion "+numero+":");*/
             /*numeros=teclado.nextDouble();*/
-            numeros = numAleatorio.nextInt(20-10)-4;
+            numeros = numAleatorio.nextInt(10)-4;
             System.out.println(numeros);
             if (numeros<0) {cantNeg++;} 
             if (numeros>0) {cantPost=cantPost+1;} 
@@ -31,7 +31,43 @@ public class EjerciciosCiclicos {
         );
     }
 
+    public static void pagoHamburgesas() {
+        //Daclarar Variables
+        String tipoPago;
+        int cantidad;
+        double totalPago=0;
+        int contador=1;
+        //proceso y datos de Entrada
+        while (contador<=3){
+            if (contador==1) {
+                System.out.println("Ingrese la cantidad de hamburgesas simples:");
+                cantidad=teclado.nextInt();
+                totalPago=totalPago+cantidad*20;
+            }
+            if (contador==2) {
+                System.out.println("Ingrese la cantidad de hamburgesas Dobles:");
+                cantidad=teclado.nextInt();
+                totalPago=totalPago+cantidad*25;
+            }
+            if (contador==3) {
+                System.out.println("Ingrese la cantidad de hamburgesas Triples:");
+                cantidad=teclado.nextInt();
+                totalPago=totalPago+cantidad*28;
+            }  
+            contador++;                  
+        }
+        //datos de salida, entrada
+        System.out.println("Ingrese la forma de pago (Con tarjeta=CT, Sin tarjeta=ST):");
+        tipoPago=teclado.next();
+        if (tipoPago.toUpperCase().equals("CT")) {
+            totalPago=totalPago+totalPago*0.05;
+        } 
+        System.out.println("El monto total a pagar es:"+totalPago);
+    }
+
+
     public static void main(String[] args) {
-        contarNumerosNegPosCero();
+        //contarNumerosNegPosCero();
+        pagoHamburgesas();
     }
 }
