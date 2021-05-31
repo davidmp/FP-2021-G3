0d272c6ea8f30f2feb7064bf3f5d9d48501c7a4a
davidmp <mamanipari@gmail.com>
2021-05-31 12:01:36 -0500

    Ejemplos recursivos

--- ProyectoSV3/src/App.java
+++ ProyectoSV3/src/App.java
@@ -1,13 +1,22 @@
 import pe.edu.upeu.SubProgramas;
+import pe.edu.upeu.recur.EjemplosRecursivos;
+import pe.edu.upeu.util.Teclado;
 
 public class App {
 
     public static void main(String[] args) throws Exception {
-        SubProgramas obj=new SubProgramas();///obj es un objeto
+       /* SubProgramas obj=new SubProgramas();///obj es un objeto
         //obj.funcionExponencial();
 
         obj.matIntCompuesto();
+        System.out.println("Holasss");*/
 
-        System.out.println("Holasss");
+        Teclado tc=new Teclado();
+
+        EjemplosRecursivos objER=new EjemplosRecursivos();
+
+        int numero=tc.leer(0, "Ingrese un numero:");
+        System.out.println("Facorial iterativo:"+objER.factorial(numero));
+        System.out.println("Facorial Recursivo:"+objER.factorialRecur(numero));
     }
 }
