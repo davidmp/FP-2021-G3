903659ffe6a7fe7165f648edcaef70373eab5fae
davidmp <mamanipari@gmail.com>
2021-06-16 16:46:51 -0500

    Sistema ventas Producto P1

--- ProyectoVentasG3/src/main/java/pe/edu/upeu/app/App.java
+++ ProyectoVentasG3/src/main/java/pe/edu/upeu/app/App.java
@@ -1,13 +1,41 @@
 package pe.edu.upeu.app;
 
 import pe.edu.upeu.gui.*;
+import pe.edu.upeu.modelo.CategoriaTO;
+import pe.edu.upeu.modelo.ProductoTO;
+import pe.edu.upeu.util.LeerTeclado;
 /**
  * Hello world!
  *
  */
 public class App {
+
+    public static void registrarCategoria(CategoriaTO categ) {
+        System.out.println("--------Registro Categoria----------");
+        System.out.println("IdCategoria: "+categ.getIdCateg()+" \tnombre: "+categ.getNombre());
+    }
+    
+    public static void registrarProducto(ProductoTO prod) {
+        System.out.println("--------Registro Producto----------");
+        System.out.println("IdCategoria: "+prod.getIdCateg()+" \tnombre: "+prod.getNombre());
+    }    
+
+    
     public static void main( String[] args ){
         System.out.println( "Hello World!" );
+        LeerTeclado lt=new LeerTeclado();
+
+        CategoriaTO categx=new CategoriaTO();        
+        categx.setIdCateg(lt.leer("", "Ingrese Id categoria: "));
+        categx.setNombre(lt.leer("", "Ingrese nombre:"));
+        registrarCategoria(categx);
+
+
+        ProductoTO prodx=new ProductoTO();        
+        prodx.setIdCateg(lt.leer("", "Ingrese Id Producto: "));
+        prodx.setNombre(lt.leer("", "Ingrese nombre:"));
+        registrarProducto(prodx);        
+
         new MainGUI();
     }
 }
