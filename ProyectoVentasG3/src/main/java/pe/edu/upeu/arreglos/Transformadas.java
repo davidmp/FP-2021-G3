559f97a9667dd970f75efa784f953b6b8609afa4
davidmp <mamanipari@gmail.com>
2021-06-23 16:27:34 -0500

    Ejemplo transformadas

--- ProyectoVentasG3/src/main/java/pe/edu/upeu/arreglos/Transformadas.java
+++ ProyectoVentasG3/src/main/java/pe/edu/upeu/arreglos/Transformadas.java
@@ -24,11 +24,34 @@ public class Transformadas {
         System.out.println("Cantidad ceros:"+cantCeros);      
         System.out.println("Cantidad positivos:"+cantPost);      
         System.out.println("Cantidad Negativos:"+cantNeg);      
-
     }
+
+    public void transformada1(int dimension, int valorElem) {
+        String[][] matriz=new String[dimension][dimension];
+        for (int f = 0; f < matriz.length; f++) {
+            for (int c = 0; c < matriz[0].length; c++) {     
+                if (c<dimension-f) {
+                    matriz[f][c]=""+(valorElem+(f+c)*(f+c+1)/2 + f); 
+                }else{
+                    matriz[f][c]="";
+                }                
+            }
+        }
+        imprimirMatriz(matriz);
+    }
+
+    public void imprimirMatriz(Object[][] matriz) {
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[0].length; j++) {
+                System.out.print(matriz[i][j]+"\t");
+            }
+            System.out.println("");
+        }
+    }  
 
     public static void main(String[] args) {
         Transformadas tObj=new Transformadas();
         tObj.contarCerosPostNegMatriz();
+        tObj.transformada1(5, 0);
     }
 }
