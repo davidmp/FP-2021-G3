a950a2c5535d7be780fa455b43acfb083af6e98c
davidmp <mamanipari@gmail.com>
2021-06-28 07:25:13 -0500

    Caso ventas

--- ProyectoVentasG3/src/main/java/pe/edu/upeu/arreglos/Transformadas.java
+++ ProyectoVentasG3/src/main/java/pe/edu/upeu/arreglos/Transformadas.java
@@ -40,6 +40,20 @@ public class Transformadas {
         imprimirMatriz(matriz);
     }
 
+    public void transformada2(int dimension, int valorElem) {
+        String[][] matriz=new String[dimension][dimension];
+        for (int f = 0; f < matriz.length; f++) {
+            for (int c = 0; c < matriz[0].length; c++) {     
+                if (c<dimension-f) {
+                    matriz[f][c]=""+(valorElem+(f+c)*(f+c+1)/2 + c); 
+                }else{
+                    matriz[f][c]="";
+                }                
+            }
+        }
+        imprimirMatriz(matriz);
+    }    
+
     public void imprimirMatriz(Object[][] matriz) {
         for (int i = 0; i < matriz.length; i++) {
             for (int j = 0; j < matriz[0].length; j++) {
@@ -53,5 +67,6 @@ public class Transformadas {
         Transformadas tObj=new Transformadas();
         tObj.contarCerosPostNegMatriz();
         tObj.transformada1(5, 0);
+        tObj.transformada2(5,0);
     }
 }
